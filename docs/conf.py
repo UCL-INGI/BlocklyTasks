76e3bb6bf537cda6275f752f6e5a74efc1f73357
CelineDknp <celine.deknop@student.uclouvain.be>
2018-07-23 17:46:55 +0200

    Fixing title of doc

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'JobBlockly'
+project = 'BlocklyTasks'
 copyright = '2018, Celine Deknop'
 author = 'Celine Deknop'
 
@@ -100,7 +100,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'JobBlocklydoc'
+htmlhelp_basename = 'BlocklyTasksdoc'
 
 
 # -- Options for LaTeX output ------------------------------------------------
@@ -127,7 +127,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'JobBlockly.tex', 'JobBlockly Documentation',
+    (master_doc, 'BlocklyTasks.tex', 'BlocklyTasks Documentation',
      'Celine Deknop', 'manual'),
 ]
 
@@ -137,7 +137,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'jobblockly', 'JobBlockly Documentation',
+    (master_doc, 'blocklytasks', 'BlocklyTasks Documentation',
      [author], 1)
 ]
 
@@ -148,7 +148,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'JobBlockly', 'JobBlockly Documentation',
-     author, 'JobBlockly', 'One line description of project.',
+    (master_doc, 'BlocklyTasks', 'BlocklyTasks Documentation',
+     author, 'BlocklyTasks', 'One line description of project.',
      'Miscellaneous'),
 ]
